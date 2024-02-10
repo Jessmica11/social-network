1bc930a3e9544e67ab993db2e5d48f3f8b60b1fa
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-02-10 13:28:30 -0500

    schema for Reaction

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,9 +1,37 @@
 // const {Schema, model} = require('mongoose');
-
+const { Schema, model } = require('mongoose');
 
 
 // const reactionSchema = new Schema(
 // reactionID, reactionBody, username, createdAt
-
+const reactionSchema = new Schema(
+  {
+    reactionID: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false
+  }
+);
 
 // module.exports = reactionSchema;
+module.exports = reactionSchema;
